dd41cde8a7efeede58aad82543eca5e92fa175d1
Cedric Darricau <cedric@avantagegraphique.net>
2012-05-15 14:54:29 +0200

    fix app.fromCookie

--- app/assets/javascripts/from_cookie.js
+++ app/assets/javascripts/from_cookie.js
@@ -1,29 +1,32 @@
-var App = App || {};
+window.App = App || {};
 
-App.fromCookie =  {
+window.App.fromCookie =  {
 
-	isNumeric: function(input) {
-		return (input - 0) == input && input.length > 0;
-	},
+  isNumeric: function(input) {
+    return (input - 0) == input && input.length > 0;
+  },
 
-	udpateDom: function(){
-		var	currentUserId = $.cookie('current_user_id'),
-			cartAmount = $.cookie('cart_amount');
+  udpateDom: function(){
+    var currentUserId = $.cookie('current_user_id'),
+      cartAmount = $.cookie('cart_amount');
 
-		if(isNumeric(currentUserId)){
-			$('#nav-bar li.logout').show();
-			$('#nav-bar li.account').show();
-		} else {
-			$('#nav-bar li.login').show();
-		}
+    if(App.fromCookie.isNumeric(currentUserId)){
+      $('#nav-bar li.logout').show();
+      $('#nav-bar li.account').show();
+      $('#nav-bar li.login').hide();
+    } else {
+      $('#nav-bar li.logout').hide();
+      $('#nav-bar li.account').hide();
+      $('#nav-bar li.login').show();
+    }
 
-		$('#nav-bar li.cart-indicator').html(cartAmount).show();
-	}
+    $('#nav-bar li.cart-indicator').html(cartAmount).show();
+  }
 };
 
 
 $(function(){
 
-	App.fromCookie.updateDom();
+  window.App.fromCookie.updateDom();
 
 });
